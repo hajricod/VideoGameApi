bd054a28584f7bbccbc5e3fd419c58bbe31c3a4d
hajricod <48984962+hajricod@users.noreply.github.com>
2025-05-15 08:37:35 +0400

    added video game migration for seeding data

--- Data/VideoGameDbContext.cs
+++ Data/VideoGameDbContext.cs
@@ -5,5 +5,55 @@ namespace VideoGameApi.Data
     public class VideoGameDbContext(DbContextOptions<VideoGameDbContext> options) : DbContext(options)
     {
         public DbSet<VideoGame> VideoGames => Set<VideoGame>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<VideoGame>().HasData(
+
+                new VideoGame
+                {
+                    Id = 1,
+                    Title = "The Legend of Zelda: Breath of the Wild",
+                    Platform = "Nintendo Switch",
+                    Developer = "Nintendo EPD",
+                    Publisher = "Nintendo",
+                },
+                new VideoGame
+                {
+                    Id = 2,
+                    Title = "The Witcher 3: Wild Hunt",
+                    Platform = "PC, PS4, Xbox One, Nintendo Switch",
+                    Developer = "CD Projekt Red",
+                    Publisher = "CD Projekt",
+                },
+                new VideoGame
+                {
+                    Id = 3,
+                    Title = "God of War",
+                    Platform = "PS4, PC",
+                    Developer = "Santa Monica Studio",
+                    Publisher = "Sony Interactive Entertainment",
+                },
+                new VideoGame
+                {
+                    Id = 4,
+                    Title = "Dark Souls III",
+                    Platform = "PC, PS4, Xbox One, Nintendo Switch",
+                    Developer = "FromSoftware",
+                    Publisher = "Bandai Namco Entertainment",
+                },
+                new VideoGame
+                {
+                    Id = 5,
+                    Title = "Hollow Knight",
+                    Platform = "PC, PS4, Xbox One, Nintendo Switch",
+                    Developer = "Team Cherry",
+                    Publisher = "Team Cherry",
+                }
+            );
+        }
     }
+
 }
---
+++ Migrations/20250515043303_seeding.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace VideoGameApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class seeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "VideoGames",
+                columns: new[] { "Id", "Developer", "Platform", "Publisher", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Nintendo EPD", "Nintendo Switch", "Nintendo", "The Legend of Zelda: Breath of the Wild" },
+                    { 2, "CD Projekt Red", "PC, PS4, Xbox One, Nintendo Switch", "CD Projekt", "The Witcher 3: Wild Hunt" },
+                    { 3, "Santa Monica Studio", "PS4, PC", "Sony Interactive Entertainment", "God of War" },
+                    { 4, "FromSoftware", "PC, PS4, Xbox One, Nintendo Switch", "Bandai Namco Entertainment", "Dark Souls III" },
+                    { 5, "Team Cherry", "PC, PS4, Xbox One, Nintendo Switch", "Team Cherry", "Hollow Knight" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "VideoGames",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "VideoGames",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "VideoGames",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "VideoGames",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "VideoGames",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
